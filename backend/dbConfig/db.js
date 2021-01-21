44bc51febfaa9deb6fc79cfe6289af94d96230cf
haykpash <hpashinyan@gmail.com>
2021-01-20 16:10:57 -0800

    heroku

--- backend/dbConfig/db.js
+++ backend/dbConfig/db.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_DB, {
+    const connecting = await mongoose.connect(process.env.MONGO_DB, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
     })
-    console.log(`db connacted: ${conn.connection.host}`.green.dim)
+    console.log(`db connacted: ${connecting.connection.host}`.green.dim)
   } catch (error) {
     console.error(`Errore: ${error.message}`.red.dim)
     process.exit(1)
